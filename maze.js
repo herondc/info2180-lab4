80c13b6ad89355a937cf5c119d7cc77f15e0016d
herondc <32229486+herondc@users.noreply.github.com>
2017-10-20 23:51:54 -0500

    third exercise-third commit

--- maze.js
+++ maze.js
@@ -1,14 +1,27 @@
- window.onload = mouseover;
-  function mouseover(){
+window.onload = mouseover;
+
+var result = true;
+
+function mouseover(){
+ 	var win =document.getElementById('end');
   	var border = document.querySelectorAll('div.boundary');
   	for (var i =0; i <5; i++) {
-	border[i].setAttribute('onmouseover','hitwalls()');
-			}
-	}
-
+  		border[i].setAttribute('onmouseover','hitwalls()');
+  		}
+  		win.setAttribute('onmouseover','end()');
+  	}
 function hitwalls(){
+	result = false;
 	var border = document.querySelectorAll('div.boundary');
 	for (var i =0; i <5; i++) {
-		border[i].setAttribute('style','background-color:red');
-	}
+			border[i].setAttribute('style','background-color:red');
+				}
+					}
+ 
+function end() {
+     if(result) {
+     	alert("You win! :)");
+    } else {
+         alert("Sorry, you lost. :(");
+    }
   } 
