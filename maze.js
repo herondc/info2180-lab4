94a26f33fb316bc6ea99771e2b963d701351b806
herondc <32229486+herondc@users.noreply.github.com>
2017-10-20 23:38:10 -0500

    first exercise-first commit

--- maze.js
+++ maze.js
@@ -1,12 +1,11 @@
 window.onload = mouseover;
 
 function mouseover(){
-	var border =document.getElementsByClassName('boundary');
- 	for (var i =0; i <5; i++){
- 		border[i].setAttribute('onmouseover','outOfBounds(this)');
+	var border =document.getElementsByClassName('boundary'); 	 
+ 		boundary1.setAttribute('onmouseover','hitwall(this)');
  	}
-};
+
  
-function outOfBounds(boundary){
-	boundary.setAttribute('style','background-color:#ff8888');
+function hitwall(boundary){
+	boundary.setAttribute('style','background-color:red');
 } 
