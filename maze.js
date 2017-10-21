1600bfd4411bb3f16f24abddbb641d8188e68d90
herondc <32229486+herondc@users.noreply.github.com>
2017-10-20 23:44:58 -0500

    second exercise - second commit

--- maze.js
+++ maze.js
@@ -1,11 +1,14 @@
-window.onload = mouseover;
+ window.onload = mouseover;
+  function mouseover(){
+  	var border = document.querySelectorAll('div.boundary');
+  	for (var i =0; i <5; i++) {
+	border[i].setAttribute('onmouseover','hitwalls()');
+			}
+	}
 
-function mouseover(){
-	var border =document.getElementsByClassName('boundary'); 	 
- 		boundary1.setAttribute('onmouseover','hitwall(this)');
- 	}
-
- 
-function hitwall(boundary){
-	boundary.setAttribute('style','background-color:red');
-} 
+function hitwalls(){
+	var border = document.querySelectorAll('div.boundary');
+	for (var i =0; i <5; i++) {
+		border[i].setAttribute('style','background-color:red');
+	}
+  } 
