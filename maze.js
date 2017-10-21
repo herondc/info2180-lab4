b21171e588bc755719375868a0276e996eb140c7
herondc <32229486+herondc@users.noreply.github.com>
2017-10-21 00:56:30 -0500

    sixth exercise - sixth commit

--- maze.js
+++ maze.js
@@ -1,38 +1,67 @@
 window.onload = mouseover;
-
-var result = true;
-
-function mouseover(){
- 	var stop = document.getElementById('end');
- 	stop.setAttribute('onmouseover','end()');
- 	var go = document.getElementById('start');
- 	go.setAttribute('onclick','start()');
-  	var border = document.querySelectorAll('div.boundary');
+  var result = true; 
+ var current = false
+ function mouseover(){
+ 	var finish = document.getElementById('end');
+ 	finish.setAttribute('onmouseover','end()');
+ var start = document.getElementById('start');
+ 	start.setAttribute('onclick','begin()');
+  
+ 	start.setAttribute('onmouseover','action()');
+ 	start.setAttribute('onmouseout','stayinmaze()');
+ var border = document.querySelectorAll('div.boundary');
+  	
   	for (var i =0; i <5; i++) {
   		border[i].setAttribute('onmouseover','hitwalls()');
-  		}
-  		   	}
-
-function hitwalls(){
-	result = false;
-	var border = document.querySelectorAll('div.boundary');
-	for (var i =0; i <5; i++) {
-			border[i].setAttribute('style','background-color:red');
-				}
-					}
+  	}
+  }
+  
+ function action() {
+ 	current = true;
+ }
+ function hitwalls(){
+  	result = false;
+  	if (current) {
+ 		var border = document.querySelectorAll('div.boundary');
+ 		document.getElementById('status').innerHTML ='You lose - Click S to refresh game';
+ 		for (var i =0; i <5; i++) {
+ 			border[i].setAttribute('style','background-color:red');
+ 		}
+  	}
+  }
+  
+  function end() {
+  	if(result && current) {
+      	document.getElementById('status').innerHTML ='You win - Click S to play again';
+      } else {
+          document.getElementById('status').innerHTML ='You lose - Click S to try again';
+      }
+  }
+  function begin() {
+  	result = true;
+  	document.getElementById('status').innerHTML ='Complete the maze';
+  	var border =document.querySelectorAll('div.boundary');
+ 	
+  	for (var i =0; i <=4; i++) {
+  		border[i].setAttribute('style','background-color: lightgrey');
+  	}
+ }
  
-function end() {
-     if(result) {
-     	document.getElementById('status').innerHTML ='You win';
-    } else {
-         document.getElementById('status').innerHTML ='YOU lose';
-    }
-  } 
-
-  function start() {
- 	result = true;
- 	var border =document.querySelectorAll('div.boundary');
- 	for (var i =0; i <=4; i++) {
- 		border[i].setAttribute('style','background-color: lightgrey');
+ function stayinmaze() {
+ 
+ 	var leavemaze =document.getElementById('maze').offsetLeft;
+ 	var mouse =event.clientX;
+ 
+ 	if(mouse < leavemaze) {
+ 		result = false;
+ 		document.getElementById('status').innerHTML ='You lose - Click S to try again';
+ 		var border = document.querySelectorAll('div.boundary');
+ 		for (var i =0; i <5; i++) {
+ 		border[i].setAttribute('style','background-color:red');
+ 		}
+ 		return false;
+ 	}
+ 	else {
+ 		return true;
  	}
   } 
